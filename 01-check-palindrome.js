3b7b035fd228f22e02eba120575bcc5d9abdc1f0
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2022-03-24 23:41:17 +0300

    fix :  comment 01-check-palindrome.js

--- 01-check-palindrome.js
+++ 01-check-palindrome.js
@@ -1,2 +1,11 @@
 const checkPalindrome = (inputString) =>
   inputString === inputString.split("").reverse().join("");
+/* 
+on as deja = complexité de split() reverse() et join() en js sont O(n) avec n la longueur du String 
+donc on a 
+1 + 1 + 1 + (1 + 1 + 1)
+O(1) + O(1) + O(1) + (O(n) + O(n) + O(n)) 
+comme O(1) << O(n) 
+
+le complexité est donc O(n)
+*/
