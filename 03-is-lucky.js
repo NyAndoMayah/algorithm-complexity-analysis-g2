063743ec016f46e8697e7ec270c333470d8c0e4a
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-03-25 06:16:04 +0300

    feat:changes in 3-is-lucky.js

--- 03-is-lucky.js
+++ 03-is-lucky.js
@@ -4,15 +4,19 @@
  * Given a ticket number n, determine if it's lucky or not.
  */
 const isLucky = (n) => {
-  if (n === 1230 || n === 134008) {
-    return true;
+  if (n === 1230 || n === 134008) {//6
+    return true;//1
   }
-  if (n === 239017) {
-    return false;
+  if (n === 239017) {//3
+    return false;//1
   }
-  const v = Math.floor(Math.random() * 2);
-  if (v === 1) {
-    return true;
+  const v = Math.floor(Math.random() * 2);//4
+  if (v === 1) {//3
+    return true;//1
   }
-  return false;
+  return false;//1
 };
+/*
+T3=6+1+3+1+4+3+1+1=20
+the algorithm's complexity is O(1) because T3 is an integer
+*/
