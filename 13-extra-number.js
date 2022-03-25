e7f44a30a53bcaed8f14d42b7857ca16e055c326
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-03-25 12:02:23 +0300

    fix:changes in 13-extra-number

--- 13-extra-number.js
+++ 13-extra-number.js
@@ -2,10 +2,18 @@
  * You're given three integers, a, b and c. It is guaranteed that two of these integers are equal to each other.
  * What is the value of the third integer ?
  */
-const extraNumber = (a, b, c) => (a === b ? c : a === c ? b : a); //9
+const extraNumber = (a, b, c) => (a === b ? c : a === c ? b : a);//9
 /*
-T13=9
-It's a number, an integer, so the algorithm's complexity is O(1)
+Ce code équivaut à:
+const extraNumber=function(a,b,c){
+  if(a === b) return c; //4
+  else if(a === c) return b; //4
+  return a //1
+} 
+
+
+T13=4+4+1=9
+It's an integer, so the complexity is O(1)
 */
 
 
