f66d1276c47946c3952c47d0940fc3e1774cca8f
NyAndoMayah <99728289+NyAndoMayah@users.noreply.github.com>
2022-03-27 18:16:11 +0300

    feat:update 06-deposit-profit.js

--- 06-deposit-profit.js
+++ 06-deposit-profit.js
@@ -5,4 +5,20 @@
  * find out how long it would take for your balance to pass a specific threshold.
  */
 const depositProfit = (deposit, rate, threshold) =>
-  Math.ceil(Math.log(threshold / deposit) / Math.log(rate / 100 + 1));
+  Math.ceil(Math.log(threshold / deposit) / Math.log(rate / 100 + 1)); //10
+/* 
+Math.ceil: accès en mémoire donc 1 opération élémentaire
+Math.log: accès en mémoire donc 1 opération élémentaire
+threshold : appel de la variable(accès à la mémoire) donc 1 opération élémentaire
+/ :opération arithmétique donc 1 opération élémentaire
+deposit:appel de la variable(accès à la mémoire) donc 1 opération élémentaire
+/ :opération arithmétique donc 1 opération élémentaire
+Math.log: accès en mémoire donc 1 opération élémentaire
+rate:appel de la variable(accès à la mémoire) donc 1 opération élémentaire
+/ :opération arithmétique donc 1 opération élémentaire
++ :opération arithmétique donc 1 opération élémentaire
+*/
+/*
+T6=1+1+1+1+1+1+1+1+1+1=10
+T6 est un nombre entier, donc la compléxité algorithmique est constante ou O(1)
+*/
